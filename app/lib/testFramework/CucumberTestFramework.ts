3bc2966e45671f8482d6be340aa19d9efcf8d415
Andy Schulz <andy.schulz@test-steps.de>
2019-01-07 00:19:22 +0100

    implement Cucumber options --format and --require

--- app/lib/testFramework/CucumberTestFramework.ts
+++ app/lib/testFramework/CucumberTestFramework.ts
@@ -1,37 +1,64 @@
-import {frame}           from "thekla-core";
 import {CucumberOptions} from "../config/ConfigProcessor";
 import {getLogger}       from "@log4js-node/log4js-api";
+import {TheklaCliOpts}   from "../thekla";
 
 export class CucumberTestFramework {
     private readonly logger = getLogger("CucumberTestFramework");
+    private requireOptions: string[] = [];
 
     private formatOptions: string[] =  [];
-    constructor(private frameworkOptions: CucumberOptions) {
+    constructor(
+        private frameworkOptions: CucumberOptions,
+        private cliOptions: TheklaCliOpts) {
 
         if(frameworkOptions) {
-            this.processFrameworkOptions(frameworkOptions);
+            this.processFrameworkOptions(cliOptions, frameworkOptions);
         }
     }
 
-    private processFrameworkOptions(frameworkOptions: CucumberOptions) {
-        if(frameworkOptions.format) {
-            if(!Array.isArray(frameworkOptions.format)) {
-                const message = `Format options should be an Array of strings but it is: ${JSON.stringify(frameworkOptions.format)}`;
-                this.logger.error(message);
-                throw new Error(message);
-            }
+    private processFrameworkOptions(cliOptions: TheklaCliOpts, frameworkOptions: CucumberOptions) {
 
-            for(let element of frameworkOptions.format) {
-                this.formatOptions.push("--format");
-                this.formatOptions.push(element);
+        this.processOptions(cliOptions.require, frameworkOptions.require, "--require");
+        this.processOptions(cliOptions.format, frameworkOptions.format, "--format");
+    }
+
+
+    private processOptions(cliOptions: undefined | string | string[], confOptions: undefined | string[], optsString: string) {
+        this.logger.debug(`processing ${optsString} option with CLI: ${cliOptions} and CONF: ${confOptions}`);
+
+        const processOptions = (options: string[]) => {
+            for(let opt of options) {
+                this.requireOptions.push(optsString);
+                this.requireOptions.push(opt);
             }
+        };
+
+        const processCliOptions = (options: string | string[]) => {
+            processOptions (Array.isArray(options) ? options : [options])
+        };
+
+        if(cliOptions && confOptions) {
+            this.logger.warn(`${optsString} are specified on command line and in the config file. I am going to use the command line options and ignore the config file.`);
+            processCliOptions(cliOptions);
+        } else if (cliOptions) {
+            processCliOptions(cliOptions);
+        } else if (confOptions) {
+            processOptions(confOptions)
         }
     }
+
 
     public run(specs: string): Promise<any> {
         this.logger.debug(`Starting Cucumber tests`);
 
         return new Promise((resolve, reject) => {
+
+            const result = (res: any) => {
+                this.logger.debug(`Cucumber: Result: ${JSON.stringify(res)}`);
+                resolve(res);
+            };
+
+
             const Cucumber = require('cucumber');
 
             const cwd = process.cwd();
@@ -44,6 +71,7 @@ export class CucumberTestFramework {
             args.push(specs);
 
             args = [...args, ...this.formatOptions];
+            args = [...args, ...this.requireOptions];
 
             this.logger.debug(JSON.stringify(Cucumber, null, "\t"));
 
@@ -54,7 +82,7 @@ export class CucumberTestFramework {
             };
 
             new Cucumber.Cli(opts).run()
-                .then(resolve)
+                .then(result)
                 .catch(reject);
         });
     }
